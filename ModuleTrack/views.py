b486b818f64a909fc3169570b3f4356e132f8f07
yhendricks@milkor.com <yhendricks@milkor.com>
2025-10-27 08:11:26 +0200

    Add bootstrapp CSS to the app

--- ModuleTrack/views.py
+++ ModuleTrack/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     return render(request, 'home.html')
 
--- ModuleTracker/settings.py
+++ ModuleTracker/settings.py
@@ -133,3 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
